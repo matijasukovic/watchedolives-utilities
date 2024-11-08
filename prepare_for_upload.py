8be3ccaa226bd3af36222619e6a06a6c3941ea71
SukovicM <suksa186@gmail.com>
2024-11-08 14:39:36 +0100

    feat: update prepare_for_upload.py to process all sets in a given directory

--- prepare_for_upload.py
+++ prepare_for_upload.py
@@ -3,21 +3,14 @@ import shutil
 import argparse
 
 parser = argparse.ArgumentParser(description='Given raw dataset obtained from WatchedOlives Pi device, generates zipped labels file for uploading labels to CVAT')
-parser.add_argument('-s','--set_path', help='Set path', required=True)
+parser.add_argument('-s','--sets_path', help='Path to the directory containing sets', default=r"W:\2024_sets\raw")
+parser.add_argument('-o', '--overwrite', help="Wheather or not to overwrite existing annotations", default="False")
 args = vars(parser.parse_args())
 
-set_path = args["set_path"]
-
-# Example input
-#set_path = r"W:\2024_sets\raw\2024_set1"
-
-
-images_dir = os.path.join(set_path, 'images')
-labels_dir = os.path.join(set_path, 'labels')
+sets_path = args["sets_path"]
+overwrite = args["overwrite"]
 
 def createAnnotationsFile(images_dir, labels_dir):
-    print("Generating annotations.txt")
-
     with open(os.path.join(labels_dir, 'annotations.txt'), 'w') as annotations_list_file:
 
         for file in os.listdir(images_dir):
@@ -34,7 +27,6 @@ def createAnnotationsFile(images_dir, labels_dir):
     annotations_list_file.close()
 
 def createObjFiles(labels_dir):
-    print("Generating obj files")
     with open(os.path.join(labels_dir, 'obj.names'), 'w') as f:
         f.write('rock')
     f.close()
@@ -46,21 +38,29 @@ def createObjFiles(labels_dir):
     f.close()
 
 def createArchive(set_path, labels_dir):
-    print('Packaging into obj_annotations_data.zip')
     shutil.make_archive(os.path.join(set_path, 'obj_annotations_data'), 'zip', labels_dir)
 
 def removeAddedFiles(labels_dir):
-    print("Cleaning up")
-
     os.remove(os.path.join(labels_dir, 'annotations.txt'))
     os.remove(os.path.join(labels_dir, 'obj.data'))
     os.remove(os.path.join(labels_dir, 'obj.names'))
 
+if __name__ == '__main__':  
+    for set_name in os.listdir(sets_path):
+        set_path = os.path.join(sets_path, set_name)
 
-createAnnotationsFile(images_dir, labels_dir)
+        uploadFile_exists = os.path.exists(os.path.join(set_path, 'obj_annotations_data.zip'))
+        if uploadFile_exists and overwrite != "True":
+            continue
 
-createObjFiles(labels_dir)
+        print(set_name)
 
-createArchive(set_path, labels_dir)
-
-removeAddedFiles(labels_dir)
+        images_dir = os.path.join(set_path, 'images')
+        labels_dir = os.path.join(set_path, 'labels')
+    
+        createAnnotationsFile(images_dir, labels_dir)
+        createObjFiles(labels_dir)
+        createArchive(set_path, labels_dir)
+        removeAddedFiles(labels_dir)
+    
+    print('Upload files generated.')
