1b8aac694cda3d5c638387ac9f683742be717eb8
Matija Šuković <82113691+matijasukovic@users.noreply.github.com>
2025-01-27 10:33:56 +0100

    feat: add 'format' argument to export.py

--- export.py
+++ export.py
@@ -1,16 +1,19 @@
 from ultralytics import YOLO
 import argparse
 
-parser = argparse.ArgumentParser(description='Description of your program')
+parser = argparse.ArgumentParser(description='Exports a PyTorch model to a desired format')
 parser.add_argument('-i','--input_path', help='Path to PyTorch (.pt) model', required=True)
+parser.add_argument('-f','--format', help='Output format', default="ncnn")
 args = vars(parser.parse_args())
 
 input_path = args["input_path"]
+format = args["format"]
 
 # Example input
 # input_path = r'W:\runs\n_p2_tuned\weights\best.pt'
+# format = 'ncnn'
 
 
 model = YOLO(input_path)
 
-model.export(format="ncnn") 
+model.export(format=format) 
