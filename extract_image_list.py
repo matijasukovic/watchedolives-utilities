b400fd4c820cda58ead75a8fde9d411a66e12f82
Matija Šuković <82113691+matijasukovic@users.noreply.github.com>
2025-01-27 10:35:28 +0100

    chore: update description of extract_image_list.py

--- extract_image_list.py
+++ extract_image_list.py
@@ -1,7 +1,7 @@
 import os
 import argparse
 
-parser = argparse.ArgumentParser(description='For a given YOLO subset, extracts a list of image filenames and saves as annotations.txt in the labels directory. This is used when uploading a YOLO dataset to CVAT, since it is needed in this process.')
+parser = argparse.ArgumentParser(description='For a given YOLO subset, extracts a list of image filenames and saves as annotations.txt in the labels directory. This is used when uploading a YOLO dataset to CVAT, in order to align with the YOLO format CVAT expects.')
 parser.add_argument('-d','--dataset_path', help='Dataset path', required=True)
 parser.add_argument('-i','--images', help='Inner path to images directory', required=True)
 parser.add_argument('-l','--labels', help='Inner path to labels directory', required=True)
