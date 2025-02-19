9f1779999cdbb131b943c957806bc8380e1bc628
matijasukovic <suksa186@gmail.com>
2025-02-19 15:17:12 +0100

    feat: add script to create dummy oil estimation dataset from image sets

---
+++ create_oil_estimation_dataset.py
@@ -0,0 +1,29 @@
+import os
+import argparse
+import random
+import shutil
+
+parser = argparse.ArgumentParser(description='Given image sets, extracts images for dummy oil estimation dataset.')
+parser.add_argument('-s','--sets_path', help='Path to the directory containing sets', default=r"/Volumes/Matija_ExtH/WatchedOlives/WatchedOlives/dataset_no_dupes")
+parser.add_argument('-a','--amount_per_set', help='How many images to extract per set', default=2)
+parser.add_argument('-o','--output_path', help='Path to the output directory', default=r"/Volumes/Matija_ExtH/WatchedOlives/WatchedOlives/dataset_oil_estimation")
+args = vars(parser.parse_args())
+
+sets_path = args["sets_path"]
+amount_per_set = args["amount_per_set"]
+output_path = args["output_path"]
+
+if __name__ == '__main__':
+    if not os.path.exists(output_path):
+        os.makedirs(output_path)
+
+    for set_name in os.listdir(sets_path):
+        set_path = os.path.join(sets_path, set_name)
+        
+        number_to_extract = amount_per_set if len(os.listdir(set_path)) > amount_per_set else len(os.listdir(set_path))
+        target_image_names = random.sample(os.listdir(set_path), k=number_to_extract)
+        
+        for image_name in target_image_names:
+            print(image_name)
+            shutil.copyfile(os.path.join(set_path, image_name), os.path.join(output_path, image_name))
+        
